828f9f58e75bc07fc8ede03bf49b394875f1d30a
Kedar Dhoble <kedardhoble2001@gmail.com>
2024-07-01 23:58:36 +0530

    Created the flask app.

---
+++ src/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+from retriever import DocumentRetriever
+from generator import ResponseGenerator
+import yaml
+
+app = Flask(__name__)
+
+# Load configuration
+with open('config/config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
+
+retriever = DocumentRetriever(config['retriever_model'], config['data_path'])
+generator = ResponseGenerator(config['generator_model'])
+
+@app.route('/query', methods=['POST'])
+def query():
+    data = request.get_json()
+    query = data['query']
+    documents = retriever.retrieve(query)
+    response = generator.generate(query, documents)
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
